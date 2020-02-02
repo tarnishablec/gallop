456281680812b0d5b2e73bd3ba65921c7a8a0668
tarnishablec <tarnishablec@outlook.com>
2020-02-02 09:45:52 +0800

    add init.js

---
+++ scripts/init.js
@@ -0,0 +1,22 @@
+const fse = require('fs-extra')
+const path = require('path')
+const args = require('minimist')(process.argv.slice(2))
+const { run } = require('./utils')
+
+const filesNeedReplace = [
+  `./scripts/setting.js`,
+  `./package.json`,
+  `./tsconfig.json`
+]
+
+main()
+
+async function main() {
+  await run(`git reset hard`)
+
+  filesNeedReplace.forEach(f => {
+    let filePath = path.resolve(__dirname, '..', f)
+    const res = fse.readFileSync(filePath, 'utf-8').replace(`~oruo~`, args.name)
+    fse.writeFileSync(filePath, res)
+  })
+}
