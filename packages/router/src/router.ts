0186685a665344cd44dc46f8dc6f9e804888cba8
tarnishablec <tarnishablec@outlook.com>
2020-08-15 23:37:48 +0800

    println!("{:?}", socket);

--- packages/router/src/router.ts
+++ packages/router/src/router.ts
@@ -1,30 +1,3 @@
-// import { HTMLClip, VirtualElement } from '@gallop/gallop'
+export interface Route {}
 
-// export type RouterOptions = {
-//   mode: 'hash' | 'normal'
-//   routes: Route[]
-//   hooks?: {
-//     beforeEach?: (from: Route, to: Route) => void
-//     afterEach?: (from: Route, to: Route) => void
-//   }
-// }
-
-// export type Route = {
-//   path: string
-//   component?:
-//     | string
-//     | HTMLClip
-//     | VirtualElement
-//     | (() => Promise<HTMLClip | VirtualElement>)
-//   exact?: boolean
-//   meta?: unknown
-//   children?: Route[]
-// }
-
-// export interface IRouter {
-//   push(): void
-//   replace(): void
-//   forward(): void
-//   go(): void
-//   back(): void
-// }
+export interface Matcher {}
