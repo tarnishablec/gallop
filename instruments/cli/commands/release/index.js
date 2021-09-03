0439c7b7126d0b387a966b09950e059a607ec686
tarnishablec <tarnishablec@outlook.com>
2021-09-03 11:21:21 +0800

    feat(gallop): change Looper.resolveCurrent => Looper.resolveCurrentElement
    BREAKING CHANGE: better static function name

--- instruments/cli/commands/release/index.js
+++ instruments/cli/commands/release/index.js
@@ -1,15 +1,23 @@
 import { run } from '../../../utils.js'
+import simpleGit from 'simple-git'
 
 export const release = async () => {
   run(`npx jest`)
   run(`yarn run build`)
-  run(`git add .`)
-  try {
-    run(`cz`)
-  } catch (error) {
-    console.log(error)
-    return
+
+  const git = simpleGit()
+  const { changed } = await git.diffSummary()
+
+  if (changed) {
+    run(`git add .`)
+    try {
+      run(`cz`)
+    } catch (error) {
+      console.log(error)
+      return
+    }
   }
+
   run(`npx lerna publish`)
   run('yarn run clean')
 }
